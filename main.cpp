0c577b357a1907d488f9055e4deb7ed442df4179
henry <xuehouyong@gmail.com>
2015-07-10 23:08:28 +0800

    testing2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << endl;
+    cout << "2222222222" << endl;
+    cout << "33333333333" << endl;
+    cout << "44444444444" << endl;
     return 0;
 }
